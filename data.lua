6b9cbf1e6093eb30abdabc3c244e4d8dd8e0f843
Caleb Heuer <ch@raiguard.me>
2025-01-28 15:24:16 -0700

    Teach miku how to sing

--- data.lua
+++ data.lua
@@ -1,16 +1,40 @@
 local sprite_frames = 30
+local sound_frames = 145
+local sprite_fps = 30 -- If this is 60 then it doesn't work... WTF?
+local overall_frames = sprite_fps * sound_frames
 
-for sprite_frame = 1, sprite_frames do
-  local item = {
-    type = "item",
-    name = "miku-" .. sprite_frame,
-    localised_name = { "item-name.miku" },
-    icon = "__dancing-miku__/graphics/items/miku/" .. ((sprite_frame % sprite_frames) + 1) .. ".png",
-    icon_size = 252,
-    stack_size = 1,
-    flags = { "not-stackable" },
-    spoil_ticks = 1,
-    spoil_result = "miku-" .. ((sprite_frame % sprite_frames) + 1),
-  }
-  data:extend({ item })
+local overall_frame = 0
+
+for sound_frame = 1, sound_frames do
+  for sprite_frame = 1, sprite_fps do
+    overall_frame = overall_frame + 1
+    local item = {
+      type = "item",
+      name = "miku-" .. overall_frame,
+      localised_name = { "item-name.miku" },
+      icon = "__dancing-miku__/graphics/items/miku/" .. ((sprite_frame % sprite_frames) + 1) .. ".png",
+      icon_size = 252,
+      hidden = true,
+      stack_size = 1,
+      flags = { "not-stackable" },
+      spoil_ticks = 1,
+      spoil_result = "miku-" .. ((overall_frame % overall_frames) + 1),
+    }
+    if sprite_frame == 1 then
+      item.spoil_to_trigger_result = {
+        trigger = {
+          type = "direct",
+          action_delivery = {
+            type = "instant",
+            source_effects = {
+              type = "play-sound",
+              sound = { filename = "__dancing-miku__/sounds/ievan-polkka/" .. sound_frame .. ".ogg", volume = 0.5 },
+            },
+          },
+        },
+        items_per_trigger = 1,
+      }
+    end
+    data:extend({ item })
+  end
 end
